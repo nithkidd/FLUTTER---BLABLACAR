81c4866aa9e3e89e64b38ef55d6f78a9b4a29dc5
nithkidd <123559135+nithkidd@users.noreply.github.com>
2026-02-05 15:43:39 +0700

    BLA-001 - Rides Service - Filter

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -1,5 +1,6 @@
 import '../data/dummy_data.dart';
 import '../model/ride/ride.dart';
+import '../model/ride/locations.dart';
 
 ////
 ///   This service handles:
@@ -8,4 +9,34 @@ import '../model/ride/ride.dart';
 class RidesService {
   static List<Ride> allRides = fakeRides;
 
+  //TODO: Complete the rides service to filter the rides starting from given departure location
+  static List<Ride> filterByDeparture(Location departure) {
+    return allRides
+        .where((ride) => ride.departureLocation == departure)
+        .toList();
+  }
+
+  // TODO: Complete the rides service to filter the rides for the given requested seat number
+  static List<Ride> filterBySeatRequested(int seatRequested) {
+    return allRides
+        .where((ride) => ride.availableSeats == seatRequested)
+        .toList();
+  }
+
+  // TODO: Complete the rides service to filter with several optional criteria (flexible filter options)
+  static List<Ride> filterBy({
+    Location? departure,
+    int? seatRequested,
+    required int seatsRequested,
+  }) {
+    return allRides.where((ride) {
+      if (departure != null && ride.departureLocation == departure) {
+        return false;
+      }
+      if (seatRequested != null && ride.availableSeats == seatsRequested) {
+        return false;
+      }
+      return true;
+    }).toList();
+  }
 }
--- test/test_rides.dart
+++ test/test_rides.dart
@@ -5,8 +5,12 @@ import 'package:blabla/services/rides_service.dart';
 
 void main() {
   Location dijon = Location(country: Country.france, name: "Dijon");
+  RidesService.filterBy(
+    departure: Location(name: "Dijon", country: Country.france),
+    seatsRequested: 2,
+  ); // Shall return 1 ride
 
-  List<Ride> filteredRide = RidesService.filter(requestedSeats: 2, departureLocation: dijon);
+  List<Ride> filteredRide = RidesService.filterBy(seatsRequested: 2, departure: dijon);
 
   for (Ride ride in filteredRide) {
     print(ride);
