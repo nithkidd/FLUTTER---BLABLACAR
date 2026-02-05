e4b2f515a73f511b87226358fa4c7948792973d3
nithkidd <123559135+nithkidd@users.noreply.github.com>
2026-02-05 14:49:31 +0700

    BLABLA - XX

---
+++ lib/services/rides_service.dart
@@ -0,0 +1,11 @@
+import '../data/dummy_data.dart';
+import '../model/ride/ride.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class RidesService {
+  static List<Ride> allRides = fakeRides;
+
+}
